9c97360780e6ebd463668534d07f08d25bda73ee
arfinaleusuf <arfinaleusuf@gmail.com>
2026-08-25 23:11:04 +0600

    feat: update App component greeting and implement Welcome component

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Welcome from './welcome.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <Welcome></Welcome> */}
+    <Welcome></Welcome>
+    <Welcome />
+
   </StrictMode>,
 )
