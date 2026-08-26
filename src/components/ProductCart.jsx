962c9fabf714e65045973085d148083e1a31cf4d
arfinaleusuf <arfinaleusuf@gmail.com>
2026-08-26 13:50:44 +0600

    feat: update ProductCart component to manage added state and enhance button styling

--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -4,12 +4,14 @@ const ProductCart = ({product}) => {
     const {name, description,price, isStock} = product
 
     const [count, setCount] = useState(0)
+    const [isAddedCart, setIsAddedCart] = useState(false)
 
     const addToCartHandler = (e) => {
         console.log(e,"e");
         e.target.textContent = 'clicked'
-        e.target.style.backgroundColor = 'red'
+        // e.target.style.backgroundColor = 'red'
         // console.log(`${name} added to cart`);
+        setIsAddedCart(!isAddedCart)
         alert(`${name} added to cart`)
     }
 
@@ -24,7 +26,7 @@ const ProductCart = ({product}) => {
             <h2>Description: {description}</h2>
             <h2>Price: {price}</h2>
             <h2>Is Stock: {isStock ? "Stock available" : "Stock Not available"}</h2>
-            <button onClick={(event) =>addToCartHandler(event)}>Add to cart</button>
+            <button onClick={(event) =>addToCartHandler(event)} className={`${isAddedCart ? "add-to-cart" : "add-to-cart-added"}`}>{isAddedCart ? "Added": "Add to cart"}</button>
             <button onClick={(event)=>buyNowHander(event)}>Buy Now</button>
             <h2>count: {count}</h2>
             <button onClick={()=>setCount(count + 1)}>Increment count</button>
